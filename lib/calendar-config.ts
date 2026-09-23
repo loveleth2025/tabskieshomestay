2ed97cfdeeaed61d607fd5686d70d052646bf7c8
loveleth2025 <87506606+loveleth2025@users.noreply.github.com>
2026-09-23 22:11:00 +0800

    Add calendar configuration for Airbnb units

---
+++ lib/calendar-config.ts
@@ -0,0 +1,6 @@
+// Configuration mapping each unit to its Airbnb calendar URL
+export const UNIT_CALENDARS: Record<string, string> = {
+  'bamboo-unit': 'https://www.airbnb.com/calendar/ical/1089385778390201817.ics?t=d35555cbdba74b2d9ca76b6acee22700',
+  'lower-unit': 'https://www.airbnb.com/calendar/ical/1115570647053269045.ics?t=a41e8ffaeee3408a83f349dd502f5a60',
+  'whole-house': 'https://www.airbnb.com/calendar/ical/1089348951544799026.ics?t=028487a1647d44b39e81221d506a2db6',
+};
